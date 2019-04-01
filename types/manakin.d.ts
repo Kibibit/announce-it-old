951659e77c402f143655e46255ab60e9981dda26
Neil Kalman <neilkalman@gmail.com>
2019-04-01 05:27:45 +0300

    add missing types + update tsconfig

---
+++ types/manakin.d.ts
@@ -0,0 +1,13 @@
+export = index;
+declare const index: {
+  error: Function;
+  global: any;
+  info: Function;
+  local: any;
+  log: Function;
+  ok: Function;
+  setBright: Function;
+  success: Function;
+  warn: Function;
+  write: Function;
+};
